478b9d3f9a450b669575ee6bd96371e9b8f6e35f
Milo Banks <milobanks@zincsoft.dev>
2021-07-21 20:00:15 -0600

    .

--- clients/MacOSCatnetClient/MacOSCatnetClient/MainViewController.swift
+++ clients/MacOSCatnetClient/MacOSCatnetClient/MainViewController.swift
@@ -14,4 +14,29 @@ class MainViewController: NSViewController {
         // Do view setup here.
     }
     
+    @IBAction func QuitButton(_ sender: Any) {
+        let alert = NSAlert()
+        alert.messageText = "Do you wish to stop CatNet?"
+        alert.informativeText = "This will disconnect you from the network!"
+        alert.alertStyle = .warning
+        alert.addButton(withTitle: "No")
+        alert.addButton(withTitle: "Yes")
+        if alert.runModal() != .alertFirstButtonReturn {
+            NSApplication.shared.terminate(self)
+        }
+    }
+}
+
+extension MainViewController {
+    // MARK: Storyboard instantiation
+    static func freshController() -> MainViewController {
+        let storyboard = NSStoryboard(name: NSStoryboard.Name("Main"), bundle: nil)
+
+        let identifier = NSStoryboard.SceneIdentifier("MainViewController")
+
+        guard let viewcontroller = storyboard.instantiateController(withIdentifier: identifier) as? MainViewController else {
+            fatalError("Why cant i find MainViewController? - Check Main.storyboard")
+        }
+        return viewcontroller
+    }
 }
